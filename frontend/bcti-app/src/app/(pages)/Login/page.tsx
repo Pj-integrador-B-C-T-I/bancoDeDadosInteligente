1438446a276b755f005989c9f06e40ff98722d8f
José Eduardo de Oliveira Jr <jjeduardo.95@gmail.com>
2025-10-01 20:36:38 -0300

    initial commit

--- frontend/bcti-app/src/app/(pages)/Login/page.tsx
+++ frontend/bcti-app/src/app/(pages)/Login/page.tsx
@@ -3,8 +3,14 @@ import AuthLayout from "@/components/shared/AuthLayout";
 export default function LoginPage() {
   return (
     <AuthLayout>
-        <div>
-            // Aqui vai o conteúdo específico da página de login ou a lateral da direita
+        <div className="flex flex-col gap-6 w-1/2">
+            
+            <h1 className="text-5xl  font-bold">Entre na sua conta</h1>
+            <form className="flex flex-col gap-4">
+                
+            </form>
+
+
         </div>
     </AuthLayout>
   );
