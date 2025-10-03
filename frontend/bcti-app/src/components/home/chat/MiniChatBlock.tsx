85808af30f17a9079656f2363a1313abffb877f4
Luciano Júnior <luciano.menezesjunior@aluno.senai.br>
2025-10-03 10:29:23 -0300

    Update the .gitignore file

--- frontend/bcti-app/src/components/home/chat/MiniChatBlock.tsx
+++ frontend/bcti-app/src/components/home/chat/MiniChatBlock.tsx
@@ -11,14 +11,14 @@ export default function MiniChatBlock() {
 
   return (
     <div className="w-full lg:w-1/2 flex flex-row p-4 gap-4">
-      <div className="grid grid-cols-1 gap-4 p-3">
-        <Link href="/Chat">
-            <div className="flex gap-2">
+      <div className="grid grid-cols-1 p-3">
+        <Link href="/Chat" className="flex flex-col gap-3">
+            <div className="flex gap-5">
               <MiniChat message={messages[0]} />
               <MiniChat message={messages[1]} />
               
           </div> 
-          <div className="flex gap-2">
+          <div className="flex gap-5">
               <MiniChat message={messages[0]} />
               <MiniChat message={messages[1]} />   
           </div> 
